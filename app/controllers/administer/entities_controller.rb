289deca15f5a79bad041556583e12d051ddbf4d1
Piotr Jakubowski <piotrj@gmail.com>
2010-12-21 00:18:25 +0100

    First implementation of generating forms for entities and new/create actions

--- app/controllers/administer/entities_controller.rb
+++ app/controllers/administer/entities_controller.rb
@@ -2,6 +2,21 @@ module Administer
   class Administer::EntitiesController < ApplicationController
     unloadable
     before_filter :collection, :only => :index
+    before_filter :fields, :only => [:new, :update]
+    
+    def new
+      @object = model_class.new
+      logger.info @fields
+    end
+    
+    def create
+      @object = model_class.new(params[model_class.model_name.underscore])
+      if @object.save
+        redirect_to administer_entities_path(:model_name => model_class.model_name)
+      else
+        render :new
+      end
+    end
     
     protected
     def model_class
@@ -11,6 +26,10 @@ module Administer
     
     def collection
       @collection = model_class.all
+    end
+    
+    def fields
+      @fields = model_class.columns.map(&:name).delete_if{ |name| ["id", "created_at", "updated_at"].any?{ |a| a == name } }
     end
   end
 end
